66d21a0c59a577bf54861f9ce13cb017d53343d4
Anthony Lim <G538003@GENMILLS.COM>
2017-03-31 15:02:28 -0500

    adding express and posts compoents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,25 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
+
+// Define the routes
+const ROUTES = [
+  {
+    path: '',
+    redirectTo: 'posts',
+    pathMatch: 'full'
+  },
+  {
+    path: 'posts',
+    component: PostsComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule,
